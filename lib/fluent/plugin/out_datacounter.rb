afd0e198fa9c0945af92bd473fce44821b390a84
negabaro <negabaro@gmail.com>
2016-02-21 02:34:53 +0900

    first

---
+++ lib/fluent/plugin/out_datacounter.rb
@@ -0,0 +1,15 @@
+require "fluent/plugin/datacounter/version"
+
+module Fluent
+  
+
+class DataCounterOutput < Fluent::BufferedOutput
+        # Your code goes here...
+end
+
+#  module Plugin
+#    module Datacounter
+      # Your code goes here...
+#    end
+#  end
+end
